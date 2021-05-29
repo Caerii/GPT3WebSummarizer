167e48d7d3575bdcf7a48a12ab3b447811316294
Pengo Wray <peter@becauseofgames.com>
2021-05-30 02:38:49 +1000

    basic start c#

---
+++ websum/websum/Gpt3Stuff.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenAI_API;
+
+namespace websum {
+    public class Gpt3Stuff {
+
+        public Gpt3Stuff() {
+            string apipath = @"Z:\openai.txt";
+            OpenAI_API.APIAuthentication.LoadFromPath(apipath);
+        }
+
+        public async void Run(string text = "One Two Three One Two") {
+
+            // https://github.com/OkGoDoIt/OpenAI-API-dotnet
+
+            var api = new OpenAI_API.OpenAIAPI(engine: Engine.Davinci);
+
+            var req = new CompletionRequest(text, temperature: 0.1);
+
+            // log req
+
+            var result1 = await api.Completions.CreateCompletionAsync(req);
+            //var result2 = await api.Completions.CreateCompletionAsync(text, temperature: 0.1);
+
+
+            Console.WriteLine(result1.ToString());
+            // should print something starting with "Three"
+        }
+    }
+}
---
+++ websum/websum/Program.cs
@@ -0,0 +1,69 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using EmbedIO;
+using EmbedIO.WebApi;
+
+
+namespace websum {
+
+    class Program {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args) {
+            var url = "http://localhost:9696/";
+            if (args.Length > 0)
+                url = args[0];
+
+            // Our web server is disposable.
+            using (var server = CreateWebServer(url)) {
+                // Once we've registered our modules and configured them, we call the RunAsync() method.
+                server.RunAsync();
+
+                var browser = new System.Diagnostics.Process() {
+                    StartInfo = new System.Diagnostics.ProcessStartInfo(url) { UseShellExecute = true }
+                };
+                browser.Start();
+                // Wait for any key to be pressed before disposing of our web server.
+                // In a service, we'd manage the lifecycle of our web server using
+                // something like a BackgroundWorker or a ManualResetEvent.
+                Console.ReadKey(true);
+            }
+        }
+
+        // Create and configure our web server.
+        private static WebServer CreateWebServer(string url) {
+
+
+            //      TODO
+
+
+
+
+            var server = new WebServer(o => o
+                .WithUrlPrefix(url)
+                .WithMode(HttpListenerMode.EmbedIO));
+            // First, we will configure our web server by adding Modules.
+            /*
+            .WithLocalSessionManager()
+            .WithWebApi("/api", m => m
+                .WithController<PeopleController>())
+            .WithModule(new WebSocketChatModule("/chat"))
+            .WithModule(new WebSocketTerminalModule("/terminal"))
+            .WithStaticFolder("/", HtmlRootPath, true, m => m
+                .WithContentCaching(UseFileCache)) // Add static files after other modules to avoid conflicts
+            .WithModule(new ActionModule("/", HttpVerbs.Any, ctx => ctx.SendDataAsync(new { Message = "Error" })));
+            */
+
+            // Listen for state changes.
+            //server.StateChanged += (s, e) => $"WebServer New State - {e.NewState}".Info();
+            server.StateChanged += (s, e) => Console.WriteLine($"WebServer New State - {e.NewState}");
+
+            return server;
+        }
+    }
+}
